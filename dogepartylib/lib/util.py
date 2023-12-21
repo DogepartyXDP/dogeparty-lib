527cdf67298389584d89fbfa70abd713cf6a9030
pataegrillo <javier@contimita.com>
2023-12-20 22:30:46 -0400

    - Now passing null to an asset issuance description will now use the description of the last issuance for that asset

--- dogepartylib/lib/util.py
+++ dogepartylib/lib/util.py
@@ -526,6 +526,17 @@ def is_divisible(db, asset):
         if not issuances: raise exceptions.AssetError('No such asset: {}'.format(asset))
         return issuances[0]['divisible']
 
+def get_asset_description(db, asset):
+    if asset in (config.DOGE, config.XDP):
+        return ''
+    else:
+        cursor = db.cursor()
+        cursor.execute('''SELECT * FROM issuances \
+                          WHERE (status = ? AND asset = ?) ORDER BY tx_index DESC''', ('valid', asset))
+        issuances = cursor.fetchall()
+        if not issuances: raise exceptions.AssetError('No such asset: {}'.format(asset))
+        return issuances[0]['description']
+
 def value_input(quantity, asset, divisible):
     if asset == 'leverage':
         return round(quantity)
